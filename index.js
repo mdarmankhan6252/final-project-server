a9c8b9aba1ff46cf0dbc2004d049cb2dc8ca7207
MD Arman khan <mdarmankhan6252@gmail.com>
2024-09-25 15:34:45 +0600

    i am committed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors())
@@ -26,8 +27,49 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
+   const userCollection = client.db("bistroDB").collection('users')
    const menuCollection = client.db("bistroDB").collection('menu')
    const reviewCollection = client.db("bistroDB").collection('review')
+   const cartCollection = client.db("bistroDB").collection('cart')
+
+   //users related api.
+   app.post('/users', async(req, res) =>{
+      const user = req.body;
+      //insert email if user doesn't exists:
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+         return res.send({message: 'User already exists', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+   })
+
+   app.get('/users', async(req, res) =>{
+      const result = await userCollection.find().toArray();
+      res.send(result)
+   })
+
+   app.patch('/users/admin/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updatedDoc = {
+         $set:{
+            role:"admin"
+         }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+   })
+
+   app.delete('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+   })
+
+   //menu related api.
 
    app.get('/menu', async(req, res) =>{
       const result = await menuCollection.find().toArray()
@@ -39,7 +81,26 @@ async function run() {
       res.send(result)
    })
 
+   //cart collection 
+   app.post('/carts', async(req, res) =>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+   })
+   app.get('/carts', async(req, res) =>{
+      const email = req.query.email;
+      const query = {email : email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+   })
 
+   app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result =await cartCollection.deleteOne(query)
+      res.send(result)
+
+   })
 
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
